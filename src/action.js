7c7a9bf6bad62ccf078d73525d6f0bd7f45d7909
Philipp Kewisch <mozilla@kewis.ch>
2020-10-16 21:47:38 +0200

    Update code for sign-addon

--- src/action.js
+++ src/action.js
@@ -7,7 +7,7 @@ import path from "path";
 import fs from "fs";
 
 import webExt from "web-ext";
-import signAddon from "sign-addon";
+import { signAddon } from "sign-addon";
 import yauzl from "yauzl-promise";
 import getStream from "get-stream";
 
